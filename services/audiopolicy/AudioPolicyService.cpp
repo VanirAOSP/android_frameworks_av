c63ffb3343db56226fb25c29df89d351569acfec
Ozzy2403 <oskar.kratochvil@gmx.de>
2015-06-18 09:03:33 +0200

    STE-Patch
    Change-Id: Id18d5bc996d32ee4d88510c57f8572b85d889bdb

--- services/audiopolicy/AudioPolicyService.cpp
+++ services/audiopolicy/AudioPolicyService.cpp
@@ -958,8 +958,11 @@ void AudioPolicyService::setParameters(audio_io_handle_t ioHandle,
                                        const char *keyValuePairs,
                                        int delayMs)
 {
+#ifdef STE_HARDWARE
+    if(strcmp("voip=on",keyValuePairs) == 0) return;
+#endif
     mAudioCommandThread->parametersCommand(ioHandle, keyValuePairs,
-                                           delayMs);
+
 }
 
 int AudioPolicyService::setStreamVolume(audio_stream_type_t stream,
